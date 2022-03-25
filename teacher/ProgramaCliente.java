d6af2694dbee1b61be77d7c873d65dc338830044
lab 05 <lab05@ACADEMICO.LOCAL>
2022-03-24 21:36:04 -0300

    workshop during class

---
+++ teacher/ProgramaCliente.java
@@ -0,0 +1,8 @@
+public class ProgramaCliente {
+    public static void main(String []args) {
+        ListaLigada<Cliente> clientes = new ListaLigada<Cliente>();
+        clientes.adicionar(new Cliente("123", "João"));
+
+        
+    }   
+}
